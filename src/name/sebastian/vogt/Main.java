90a11bfe9cf8c29e4faecbfb9be0a981b9be8592
Sebastian Vogt <s.vogt@cam-store.eu>
2016-10-06 17:27:07 +0200

    Adding some comments in Main

--- src/name/sebastian/vogt/Main.java
+++ src/name/sebastian/vogt/Main.java
@@ -13,14 +13,25 @@ public class Main {
         sugar = 11;
 
         // (gramm auf der Packung z.b. 100, gramm KH, gramm wieviel gegessen, BE oder KHE)
-        beCalculator = new BECalculator(100, 24, 200, BECalculator.BE);
+        beCalculator = new BECalculator(
+                100,
+                24,
+                200,
+                BECalculator.BE
+        );
         System.out.println("BE = " + beCalculator.compute());
 
         targetRange = new Range(4, 9);
         /*System.out.println(range.inRange(6));
         System.out.println(range.inRange(11));*/
 
-        insulinCalculator = new InsulinCalculator(sugar, 1.5, correctionFactor, targetRange.getStop(), beCalculator.compute());
+        insulinCalculator = new InsulinCalculator(
+                sugar, // aktueller Blutzucker
+                1.5, // Insulinfaktor
+                correctionFactor, // Korrekturfaktor (um wieviel wird der Blutzucker mit 1 IE Bolus Insulin gesenkt
+                targetRange.getStop(), // Ziel Blutzucker
+                beCalculator.compute() // BE oder KHE
+        );
         System.out.println("Bolus IE = " + insulinCalculator.computeBolusInsulin());
         System.out.println("Korrektur IE = " + insulinCalculator.computeCorrectionInsulin());
         System.out.println("Gesamt IE = " + insulinCalculator.computeTotalInsulin());
